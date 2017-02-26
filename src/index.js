c8eba26557b0b1e82f75e2ee132165ef28edd73c
divyanshu97 <reachdivyanshu@gmail.com>
2017-02-25 23:43:55 -0800

    Google login implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+import Login from './Login.js';
+
+function handleSuccess(data) 
+{
+	const accessToken = data.accessToken
+
+	fetch('http://direct-me.herokuapp.com/user/social/google-oauth2/', 
+	{
+		method: 'POST',
+		body: JSON.stringify({'access_token': accessToken}),
+		headers: 
+		{
+   		 "Content-Type": "application/json"
+  		}
+	})
+	.then(function(response) 
+	{
+  		return response.text()
+	})
+	.then(function(json) 
+	{
+		const user_token = json
+		
+	}),
+	function(err) 
+	{
+
+	}
+}
+
+function handleFaliure(data) 
+{
+	console.log("Error :- "+data.error)
+}
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-)
+  <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Google Login"
+    onSuccess={handleSuccess}
+    onFailure={handleFaliure}
+  />,
+  document.getElementById('root')
+);
