6b1c38fd376d209e35d7891777e394efe649d3ed
divyanshu97 <reachdivyanshu@gmail.com>
2017-02-26 11:37:34 -0800

    All logs removed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
-//import {Router, Route, browserHistory} from 'react-router';
 import Login from './Login.js';
+
 const responseGoogle = (response) => {
-  console.log(response)
+
 }
 function handleSuccess(data) {
 	const accessToken = data.accessToken
-	console.log("Sending data")
-	console.log(data.accessToken)
+
 	fetch('http://direct-me.herokuapp.com/user/social/google-oauth2/', {
 		method: 'POST',
 		body: JSON.stringify({'access_token': accessToken}),
@@ -17,17 +16,16 @@ function handleSuccess(data) {
    		 "Content-Type": "application/json"
   		}
 	}).then(function(response) {
-		console.log(response.json())
+
 	}).then(function(json) {
 
 	}),function(err) {
-		console.log("Some error occured")
-		console.log("Error : "+err.Message)
+
 	}
 }
 
 function handleFaliure(data) {
-	console.log("Error :- "+data.error)
+
 }
 
 ReactDOM.render(
