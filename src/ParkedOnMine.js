019f5516c1fb149e25137bb1766fce0ab114908e
Sukankshi <sukankshi.jain@gmail.com>
2017-03-03 11:29:16 +0530

    Parked on mine implemented (#10)

--- src/ParkedOnMine.js
+++ src/ParkedOnMine.js
@@ -7,7 +7,7 @@ let ParkedOnMine = React.createClass({
 
     getInitialState(){
         return {
-            user_list: []
+            port_list: []
         }
     },
     componentDidMount(){
@@ -26,24 +26,24 @@ let ParkedOnMine = React.createClass({
                 let port = json[i]
                 if (port.logs.length) {
                     data.push({
-                        type: port.type,
-                        id: port.id,
+                        port_type: port.type,
+                        port_id: port.id,
                         user_name: port.logs[0].username,
                         user_id: port.logs[0].user_id,
                         ship_image: base_url + port.logs[0].ship_image,
-                        vacant: 0
+                        is_vacant: 0
                     })
                 }
                 else {
                     data.push({
-                        type: port.type,
-                        id: port.id,
-                        vacant: 1
+                        port_type: port.type,
+                        port_id: port.id,
+                        is_vacant: 1
                     })
                 }
             }
             self.setState({
-                user_list: data
+                port_list: data
             })
         }).catch(function (ex) {
             console.log('parsing failed', ex)
@@ -53,15 +53,15 @@ let ParkedOnMine = React.createClass({
         return (
             <div>
                 {
-                    this.state.user_list.map((port,i) => {
+                    this.state.port_list.map((port,i) => {
                         return (
                             <div>
-                                <p> {port.type} </p>
-                                <p> {port.id} </p>
+                                <p> {port.port_type} </p>
+                                <p> {port.port_id} </p>
                                 <p> {port.user_name} </p>
                                 <p> {port.user_id} </p>
                                 <img style={{height: 200, width: 200}} src={port.ship_image}/>
-                                <FineButton port_id={port.id} show={port.type == "Parking" || (port.vacant && port.type=="Non Parking")}/>
+                                <FineButton port_id={port.port_id} show={port.port_type == "Parking" || (port.is_vacant && port.port_type=="Non Parking")}/>
                                 <hr/>
                             </div>
                         )
